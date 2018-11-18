6c2a17452ee2430ace352b89f09d68d433801bb4
Darren <dk.darrenkeen@gmail.com>
2018-11-18 20:56:35 +0000

    Completed 9

--- frontend/components/nav.js
+++ frontend/components/nav.js
@@ -1,17 +1,24 @@
 import Link from 'next/link';
+import NavStyled from './styles/NavStyles';
 
-const Nav = () => {
-  return (
-    <div>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/sell">
-        <a>Sell</a>
-      </Link>
-
-    </div>
-  );
-};
+const Nav = () => (
+  <NavStyled>
+    <Link href="/items">
+      <a>Items</a>
+    </Link>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+    <Link href="/signup">
+      <a>Sign up</a>
+    </Link>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Me</a>
+    </Link>
+  </NavStyled>
+);
 
 export default Nav;
