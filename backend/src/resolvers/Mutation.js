011ac2be8910b45fa3926a6a46967266304f4d99
Darren <dk.darrenkeen@gmail.com>
2018-12-03 18:52:57 +0000

    Completed 13

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    //create a dog
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+  }
+};
 
-module.exports = mutations;
+module.exports = Mutations;
